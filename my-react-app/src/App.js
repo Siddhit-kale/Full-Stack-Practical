710571494ab9cd3829f3e036a6be9eef39541072
Kale Siddhit <159001710+Siddhit-kale@users.noreply.github.com>
2024-07-31 21:52:04 +0530

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
+import Counter from './components/Counter';
+import Greet from './components/Greet';
+import Message from './components/Message';
+import Welcome from './components/Welcome';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+      <Greet />
+      <Message />
+      <Welcome />
     </div>
   );
 }
